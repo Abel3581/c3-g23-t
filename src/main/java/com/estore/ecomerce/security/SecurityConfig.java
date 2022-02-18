2c8d0dee164829aaf9c1157572aa94ed4384c434
Abel3581 <marzoa3581@gmail.com>
2022-02-18 18:31:06 -0300

    Actualizacion con main

--- src/main/java/com/estore/ecomerce/security/SecurityConfig.java
+++ src/main/java/com/estore/ecomerce/security/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST,"/auth/login").permitAll()
                 .antMatchers(HttpMethod.GET,"/auth/me").permitAll()
                 .antMatchers(HttpMethod.POST,"/api/category").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/category/{id}").permitAll()
                 .antMatchers(HttpMethod.DELETE,"/user/{id}").permitAll()
                 .anyRequest()
                 .authenticated()
