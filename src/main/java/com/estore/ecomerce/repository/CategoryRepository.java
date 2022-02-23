1349fd0508a17ef0a6a41ec31b18bd661fd67fa4
Gabriel Navarro <85877669+gabynavarro@users.noreply.github.com>
2022-02-23 12:06:52 -0300

    modificacón

--- src/main/java/com/estore/ecomerce/repository/CategoryRepository.java
+++ src/main/java/com/estore/ecomerce/repository/CategoryRepository.java
@@ -1,18 +1,18 @@
-
 package com.estore.ecomerce.repository;
 
 import com.estore.ecomerce.domain.Category;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends BaseRepository<Category, Long> {
-     @Query("SELECT c from Category c WHERE c.status=true")
-     public List<Category> listCategoryActive();
-     @Query("SELECT c from Category c WHERE c.status=false")
-     public List<Category> listCategoryInactive();
-//    @Query("SELECT a from Category a WHERE a.id= :id")
-//    public Category searchCategoryId(@Param("id") String id);
-    
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query("SELECT c from Category c WHERE c.status=true")
+    public List<Category> listCategoryActive();
+
+    @Query("SELECT c from Category c WHERE c.status=false")
+    public List<Category> listCategoryInactive();
+
 }
