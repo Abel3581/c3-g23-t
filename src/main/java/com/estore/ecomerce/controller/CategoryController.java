6d2a1ff17b41aae54528a2c7c6859ae5f7ac0d00
Gabriel Navarro <85877669+gabynavarro@users.noreply.github.com>
2022-02-23 12:00:36 -0300

    solucion problema

--- src/main/java/com/estore/ecomerce/controller/CategoryController.java
+++ src/main/java/com/estore/ecomerce/controller/CategoryController.java
@@ -1,15 +1,24 @@
 
 package com.estore.ecomerce.controller;
 
-import com.estore.ecomerce.domain.Category;
-import com.estore.ecomerce.service.CategoryServiceImpl;
+import com.estore.ecomerce.dto.CategoryRequest;
+import com.estore.ecomerce.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin( origins = "*") 
-@RequestMapping("/api/v1/category")
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "/api/v1/category")
 public class CategoryController {
 
     @Autowired
@@ -79,3 +88,4 @@ public class CategoryController {
     }
 
 }
+
