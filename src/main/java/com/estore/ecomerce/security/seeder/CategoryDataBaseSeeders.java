73364ec6a2f5241fed8f7c2d1361208d98633d7f
gabynavarro <alt64b@gmail.com>
2022-03-04 15:01:16 -0300

    Seeder Category
    Auto carga de imágenes.

--- src/main/java/com/estore/ecomerce/security/seeder/CategoryDataBaseSeeders.java
+++ src/main/java/com/estore/ecomerce/security/seeder/CategoryDataBaseSeeders.java
@@ -1,53 +1,61 @@
-
 package com.estore.ecomerce.security.seeder;
 
 import com.estore.ecomerce.domain.Category;
 import com.estore.ecomerce.domain.ImageProfile;
 import com.estore.ecomerce.repository.CategoryRepository;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class CategoryDataBaseSeeders {
-    private static final String categories[]= {"Calzado", "Indumentaria", "Hogar", "Electro","Perfume"};
+
+    private static final String categories[] = {"Calzado", "Indumentaria", "Hogar", "Electro", "Perfume"};
 
     @Autowired
-    private CategoryRepository categoryRepository ;
-   
+    private CategoryRepository categoryRepository;
 
     @EventListener
-    public void seed(ContextRefreshedEvent event) {
-        List<Category> category = categoryRepository.findAll();
-        if (category.isEmpty()) {
-            createCategory();
-        }
-    }
-    
-    private void createCategory() { 
-        String sCarpAct = System.getProperty("user.dir");
-        File file = new File(sCarpAct+"/src/main/resources/static/images/");  //carpeta donde se ejecuta codigo
-        ImageProfile image=new ImageProfile();
-        
-        File[] listFile = file.listFiles();
-            for (int index = 0; index<categories.length; index++) {
-            Category category = new Category();
-            category.setName( categories[index].toUpperCase());
-            category.setDescription("Descripción de "+categories[index]);
-            category.setProducts(null);
-            category.setImageProfile((convertFile(listFile[index])));
-            category.setStatus(Boolean.TRUE);            
-            categoryRepository.save(category);
+    public void seed(ContextRefreshedEvent event) throws IOException {
+        List<Category> categorylis = categoryRepository.findAll();
+
+        if (categorylis.isEmpty()) {
+
+            String sCarpAct = System.getProperty("user.dir");
+            File file = new File(sCarpAct + "/src/main/resources/static/images/");  //carpeta donde se ejecuta codigo
+
+            File[] listFile = file.listFiles();
+            for (int index = 0; index < categories.length; index++) {
+                ImageProfile image = new ImageProfile();
+                Category category = new Category();
+                category.setName(categories[index].toUpperCase());
+                category.setDescription("Descripción de " + categories[index]);
+
+                InputStream inputStream = new FileInputStream(listFile[index]);
+                //String name, @Nullable String originalFilename, @Nullable String contentType, InputStream contentStream)
+                MultipartFile partFile = new MockMultipartFile(listFile[index].getName().toString(), listFile[index].getName().toString(), "image/png", inputStream);
+
+                image.setFileData(partFile.getBytes());
+                image.setFileType("image/png");
+                image.setName(partFile.getName());
+                category.setImageProfile(image);
+                category.setStatus(Boolean.TRUE);
+                category.setProducts(null);
+                categoryRepository.save(category);
+
+            }
         }
     }
 
-    private ImageProfile convertFile(File file) {
-       
-        
-            return null;
-    }
-  
+   
 }
+
+
